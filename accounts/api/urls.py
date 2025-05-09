224bb03dbee5a319151c57bccc34f8b0f1e0d736
TheK <zeka.2006huseynli@gmail.com>
2025-05-09 14:10:19 +0400

    login and register templates

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -4,7 +4,7 @@ from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 from .views import RegisterView, LoginView
 urlpatterns = [
     path('users/', UserListView.as_view(), name='user-list'),
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
+    path('register/', RegisterView.as_view(), name='register-api'),
+    path('login/', LoginView.as_view(), name='login-api'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,13 +3,14 @@ from django.contrib import messages
 from .forms import RegisterForm, LoginForm
 from .models import CustomUser
 from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
 
 def register_view(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
-            user.set_password(form.cleaned_data['password'])  # Hash the password
+            user.set_password(form.cleaned_data['password']) 
             user.save()
             messages.success(request, "Registration successful. Please log in.")
             return redirect('login')
@@ -31,7 +32,7 @@ def login_view(request):
             if user is not None:
                 login(request, user)
                 messages.success(request, 'Logged in successfully!')
-                return redirect('user-list')  # change this to any view you like
+                return redirect('home')  # change this to any view you like
             else:
                 messages.error(request, 'Invalid email or password.')
     else:
@@ -40,3 +41,8 @@ def login_view(request):
             'form': form,
         }
     return render(request, 'registration/login.html', context)
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, 'Logged out successfully!')
+    return redirect('home')  
